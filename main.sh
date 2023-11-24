db6ef07e276d364662891bd3a43a428f5e4777e5
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2023-11-24 18:31:05 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -132,7 +132,6 @@ TIME=$(TZ='Europe/Berlin' date +"%Y-%m-%d %H:%M:%S")
 ZIP_NAME="$KERNEL_NAME.zip"
 find ./ * -exec touch -m -d "$TIME" {} \;
 zip -r9 $ZIP_NAME *
-mkdir -p $WORKDIR/out/artifacts
 cp *.zip $WORKDIR/out/artifacts
 
 cd $WORKDIR/out
