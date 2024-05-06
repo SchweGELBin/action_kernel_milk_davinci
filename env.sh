9267d8a7161f9978e3e26f185074c98665a7d0d7
SchweGELBin <67663319+SchweGELBin@users.noreply.github.com>
2024-05-06 22:52:00 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -6,4 +6,3 @@ apt autoremove
 
 # Environment Setup
 apt-get update && apt-get upgrade -y
-apt-get install default-jdk git-core gnupg flex bison gperf build-essential zip curl libc6-dev libncurses5-dev x11proto-core-dev libx11-dev libreadline6-dev libgl1-mesa-glx libgl1-mesa-dev python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev libncurses5 python-is-python3 libc6-dev libtinfo5 ncurses-dev make python2 repo cpio kmod openssl -y
